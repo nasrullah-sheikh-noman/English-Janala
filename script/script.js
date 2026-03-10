e73f50b2c703cd95739d5260d65a47816fc695b0
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-03-10 09:37:46 +0600

    add modal card

--- script/script.js
+++ script/script.js
@@ -28,6 +28,41 @@ const loadLevelWord = (id) => {
   })
 }
 
+
+const displayModalContainer = (id) => {
+
+  fetch(`https://openapi.programming-hero.com/api/word/${id}`)
+    .then(res => res.json())
+    .then(details => {
+      showModal(details.data)
+      }
+    )
+
+
+}
+
+
+const showModal = (info) => {
+  const modalContainer = document.getElementById("modal-info");
+
+  modalContainer.innerHTML = `
+    <h2>Eager (     :ইগার)</h2>
+    <h4>Meaning</h4>
+    <h4>আগ্রহী</h4>
+    <h3>Example</h3>
+    <p>The kids were eager to open their gifts.</p>
+    <p>সমার্থক শব্দ গুলো</p>
+    <button>Enthusiastic</button>
+    <button>excited</button>
+    <button>keen</button>
+    <button>Complete Learning</button>
+  `;
+
+  document.getElementById("modal_container").showModal();
+}
+
+
+
 const displayLevelWord = (levels) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
@@ -46,14 +81,23 @@ const displayLevelWord = (levels) => {
   levels.forEach(level => {
     const cardDiv = document.createElement("div");
     cardDiv.innerHTML = `
+
     <div class="bg-white rounded-sm space-y-3 py-12 px-5 gap-5 shadow-sm text-center h-full ">
+
       <h2 class="text-2xl font-semibold">${level.word ? level.word : "শব্দ  পাওয়া যায়নি"}</h2>
+
       <p class="text-lg">Meaning / Pronounciation</p>
+
       <p class="text-2xl font-bold">${level.meaning  ? level.meaning : "অর্থ পাওয়া যায়নি"} / ${level.pronunciation ? level.pronunciation : "উচ্চারণ পাওয়া যায়নি"}</p>
+
       <div class="flex justify-between">
-        <button class="btn bg-[#1A91FF10]"><i class="fa-solid fa-circle-info"></i></button>
+
+        <button onclick="displayModalContainer(${level.id})" class="btn bg-[#1A91FF10]"><i class="fa-solid fa-circle-info"></i></button>
+
         <button class="btn bg-[#1A91FF10]"><i class="fa-solid fa-volume-high"></i></button>
+
       </div>
+
     </div>
     `
     wordContainer.append(cardDiv);
